ac67b8e8527ca1f961e1524b9bd9060240759572
KooWihC <ahnchiwook@hanmail.net>
2025-04-11 23:26:02 +0900

    [Feat] 1주차 세미나 실습~

---
+++ AT-SOPT-iOS-Practice/AT-SOPT-iOS-Practice/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  week1
+//
+//  Created by 안치욱 on 4/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    private var testView: UIView = UIView(frame: .init(origin: .init(x: 128, y: 100), size: .init(width: 128, height: 256)))
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = .white
+//        testView.backgroundColor = .systemPurple
+//        
+//        self.view.addSubview(testView)
+        
+    }
+}
+
